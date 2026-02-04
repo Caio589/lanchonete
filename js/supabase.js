2612a3f30f161386335a141bcbda352bd4b91fd6
C.O Sistemas <caiosan321@icloud.com>
2026-02-03 21:57:54 -0300

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
+
+const supabaseUrl = "https://kglmusilzoixvqfgyvlv.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtnbG11c2lsem9peHZxZmd5dmx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1NTIzMTUsImV4cCI6MjA4NTEyODMxNX0.scUytKXFHaNI1VZcHSzUmKoqpMKCKGxm7RXquirHNHc"
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
