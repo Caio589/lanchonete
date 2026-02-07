111763571fd36671d640ee8e3d8b1b2d2fe772f0
C.O Sistemas <caiosan321@icloud.com>
2026-02-07 12:40:10 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -4,3 +4,4 @@ const supabaseUrl = "https://kglmusilzoixvqfgyvlv.supabase.co"
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtnbG11c2lsem9peHZxZmd5dmx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1NTIzMTUsImV4cCI6MjA4NTEyODMxNX0.scUytKXFHaNI1VZcHSzUmKoqpMKCKGxm7RXquirHNHc"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
+window.supabase = supabase; // ⭐ LINHA CRÍTICA
